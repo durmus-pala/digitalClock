571eae743365847a2bfa9669231fc96f87836ed4
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-05-04 11:32:45 +0300

    add getDate substr to make it 2 digits

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const myDate = document.getElementById('date');
 function myTime() {
     let rightNow = new Date();
     let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    myDate.innerHTML = `${('0' + rightNow.getDate())} / ${('0' + (rightNow.getMonth() + 1)).substr(-2)} / ${rightNow.getFullYear()}<br>${weekdays[rightNow.getDay()]}`; 
+    myDate.innerHTML = `${('0' + rightNow.getDate()).substr(-2)} / ${('0' + (rightNow.getMonth() + 1)).substr(-2)} / ${rightNow.getFullYear()}<br>${weekdays[rightNow.getDay()]}`; 
     myClock.innerHTML = `${('0' + rightNow.getHours()).substr(-2)} : ${('0' + rightNow.getMinutes()).substr(-2)} : ${('0' + rightNow.getSeconds()).substr(-2)}`;
 }
 
